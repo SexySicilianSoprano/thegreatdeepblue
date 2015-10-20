30620fefa6105115ddc2b8a3356806e8d2bfd6b8
SexualTension <n4saka00@students.oamk.fi>
2015-10-20 15:47:10 +0300

    Update #2
    - Player
    - SetPlayer
    - Weapon scripts (merely files)
    -Skouttiboutti

---
+++ The Great Deep Blue/Assets/Scripts - In Game/Core/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player {
+
+    public int playerID {
+        get;
+        set;
+    }
+
+    public string playerScreenName {
+        get;
+        set;
+    }
+
+    public Color playerColor {
+        get;
+        set;
+    }
+
+    public int playerTeam {
+        get;
+        set;
+    }
+    
+}
---
+++ The Great Deep Blue/Assets/Scripts - Menus/SetPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPlayer : MonoBehaviour {
+    
+    public Player Player1 = new Player()
+    {
+        playerID = 1,
+        playerScreenName = "Mingebag",
+        playerColor = new Color (255,0,0),
+        playerTeam = 1
+        
+    };
+
+    public Player Player2 = new Player()
+    {
+        playerID = 2,
+        playerScreenName = "TestEnemy",
+        playerColor = new Color(0, 0, 255),
+        playerTeam = 2
+
+    };
+
+
+
+}
