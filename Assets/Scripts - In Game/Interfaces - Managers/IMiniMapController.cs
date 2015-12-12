7000dc300a204b1fd635166e0754081f552fad67
lavil <vilhunen.laura@outlook.com>
2015-12-12 16:16:43 +0200

    emptying

--- Assets/Scripts - In Game/Interfaces - Managers/IMiniMapController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public interface IMiniMapController {
-
-	void LoadMiniMap(out float guiWidth, out Rect miniMapRect);
-	void ReCalculateViewRect();
-}
