68977ae9df3a085417248e1bed2b47fec12304e1
lavil <vilhunen.laura@outlook.com>
2016-01-12 16:13:42 +0200

    UI update!
    Start of Info Box and Cursors

---
+++ Assets/InfoBox.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InfoBox : MonoBehaviour {
+
+	public GameObject Info;
+	public Button Btn;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+	}
+
+	void MousePosition () {
+		if  (Input.mousePosition == Btn.transform.position ) {
+			Info.SetActive(true);
+		}
+		else {
+			Info.SetActive(false);
+		}
+	}
+}
---
+++ Assets/NewCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NewCursor : MonoBehaviour {
+
+	public RawImage cursor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+	}
+}
